50d0c7a982fad53689ea143a657e2526c96063d0
atrifyllis <atrifyllis@gmail.com>
2022-05-13 23:55:37 +0300

    Add initial integration test using testcontainers to check loaded reference data.

--- src/test/kotlin/gr/senik/netcalculator/application/service/ReferenceDataServiceIntegrationTest.kt
+++ src/test/kotlin/gr/senik/netcalculator/application/service/ReferenceDataServiceIntegrationTest.kt
@@ -1,7 +1,11 @@
 package gr.senik.netcalculator.application.service
 
+import gr.senik.common.domain.model.Money
 import gr.senik.netcalculator.IntegrationTestBase
 import gr.senik.netcalculator.domain.model.insurance.EfkaClassType
+import gr.senik.netcalculator.domain.model.insurance.EteaepClassType
+import gr.senik.netcalculator.domain.model.tax.SolidarityContributionLevelType
+import gr.senik.netcalculator.domain.model.tax.TaxLevelType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,6 +21,16 @@ internal class ReferenceDataServiceIntegrationTest : IntegrationTestBase() {
         val (eteaepClasses, efkaClasses, incomeTaxLevels, solidarityContributionTaxLevels) =
             referenceDataService.getReferenceData()
 
-        assertThat(eteaepClasses[0].type).isEqualTo(EfkaClassType.FIRST)
+        assertThat(eteaepClasses[0].type).isEqualTo(EteaepClassType.FIRST)
+        assertThat(eteaepClasses[0].lumpSumAmount).isEqualTo(Money(26))
+
+        assertThat(efkaClasses[1].type).isEqualTo(EfkaClassType.SECOND)
+        assertThat(efkaClasses[1].mainPensionAmount).isEqualTo(Money(186))
+
+        assertThat(incomeTaxLevels[2].type).isEqualTo(TaxLevelType.THIRD_10K)
+        assertThat(incomeTaxLevels[2].levelFactor).isEqualTo(28.0)
+
+        assertThat(solidarityContributionTaxLevels[4].type).isEqualTo(SolidarityContributionLevelType.FIFTH_25K)
+        assertThat(solidarityContributionTaxLevels[4].levelLimit).isEqualTo(Money(25_000))
     }
 }
