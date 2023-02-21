55d2c037b855e3443d4b1afd560c200492127c58
atrifyllis <a.trifyllis@gmail.com>
2023-02-21 19:49:14 +0200

    Move caheable annotations to secondary adapters, ports should be technology agnostic and also if class based proxy is used annotations are not transferred from interfaces.

--- src/main/kotlin/gr/senik/netcalculator/application/ports/out/LoadEteaepClassesPort.kt
+++ src/main/kotlin/gr/senik/netcalculator/application/ports/out/LoadEteaepClassesPort.kt
@@ -1,9 +1,7 @@
 package gr.senik.netcalculator.application.ports.out
 
 import gr.senik.netcalculator.domain.model.EteaepClass
-import org.springframework.cache.annotation.Cacheable
 
 sealed interface LoadEteaepClassesPort {
-    @Cacheable("eteaepClasses")
     fun loadEteaepClasses(): List<EteaepClass>
 }
--- src/main/kotlin/gr/senik/netcalculator/application/ports/out/LoadSolidarityContributionTaxLevelsPort.kt
+++ src/main/kotlin/gr/senik/netcalculator/application/ports/out/LoadSolidarityContributionTaxLevelsPort.kt
@@ -2,10 +2,8 @@ package gr.senik.netcalculator.application.ports.out
 
 import gr.senik.netcalculator.domain.model.SolidarityTax
 import gr.senik.netcalculator.domain.model.TaxLevel
-import org.springframework.cache.annotation.Cacheable
 
 sealed interface LoadSolidarityContributionTaxLevelsPort {
-    @Cacheable("solidarityContributionTaxLevels")
     fun loadSolidarityContributionTaxLevels(): List<TaxLevel>
 
     fun loadSolidarityContributionTax(): SolidarityTax
