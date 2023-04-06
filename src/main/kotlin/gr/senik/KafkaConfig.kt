b2ee85d212f4c4778f9196d556a9a98043600894
atrifyllis <a.trifyllis@gmail.com>
2023-04-06 23:52:16 +0300

    Configure custom DefaultKafkaConsumerFactory with custom JsonDeserializer to be able to use Spring Boots's object mapper and in turn Jackson Mixins. Could not find a better way to have a generic consumer factory and be able to deserialize DomainEvents using the @JsonTypeInfo annotation, but I had to specify DomainEvent in here.

--- src/main/kotlin/gr/senik/KafkaConfig.kt
+++ src/main/kotlin/gr/senik/KafkaConfig.kt
@@ -1,17 +1,26 @@
 package gr.senik
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import gr.alx.common.domain.model.DomainEvent
 import jakarta.validation.ValidationException
 import mu.KotlinLogging
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaConsumerFactoryCustomizer
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.annotation.KafkaListenerConfigurer
 import org.springframework.kafka.config.KafkaListenerEndpointRegistrar
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer
 import org.springframework.kafka.listener.DefaultErrorHandler
 import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
+import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 
 
@@ -24,8 +33,8 @@ private val log = KotlinLogging.logger {}
 @Configuration
 @EnableKafka
 class KafkaConfig(
-    @Autowired
-    private var validator: LocalValidatorFactoryBean,
+        @Autowired
+        private var validator: LocalValidatorFactoryBean,
 ) : KafkaListenerConfigurer {
 
     /**
@@ -50,4 +59,23 @@ class KafkaConfig(
         errorHandler.addNotRetryableExceptions(ValidationException::class.java)
         return errorHandler
     }
+
+
+    /**
+     * Creates custom JsonDeserializer that uses Spring Boot's objectMapper and TypeReference which results in usage of
+     * the {@see DomainEventMixIn}. Moreover, it is wrapped by ErrorHandlingDeserializer, it was the only way to
+     * leverage the DefaultErrorHandler configured above.
+     *
+     *TODO Could not find a way to use spring boot objectMapper, which relies on the DomainMixin to ser/deserialize event.
+     */
+
+    @Bean
+    fun kafkaConsumerFactory(customizers: ObjectProvider<DefaultKafkaConsumerFactoryCustomizer>, properties: KafkaProperties, objectMapper: ObjectMapper): DefaultKafkaConsumerFactory<*, *> {
+        val factory = DefaultKafkaConsumerFactory<String, DomainEvent>(properties.buildConsumerProperties())
+        customizers.orderedStream().forEach { customizer: DefaultKafkaConsumerFactoryCustomizer -> customizer.customize(factory) }
+        factory.setValueDeserializer(ErrorHandlingDeserializer(JsonDeserializer(object : TypeReference<DomainEvent>() {}, objectMapper)))
+        return factory
+    }
+
+
 }
